382a8ae31005384b22025db8d46da98e2f6a9926
Cichacz <m.cichacz@gmail.com>
2018-03-25 17:16:48 +0200

    Konfiguracja ts + less

---
+++ src/components/index/Index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container">
+        <h1>Wycieczkownia</h1>
+        <h2>Aplikacja do planowania wycieczek</h2>
+    </div>
+</template>
+
+<script src="./component.ts" lang="ts"></script>
+<style src="./style.less" lang="less"></style>
