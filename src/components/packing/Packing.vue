8ece81101f977d34d676756198b1930227bdc716
Damian Pieczara <damian1995pieczara@gmail.com>
2018-05-26 23:36:20 +0200

    BSVue/PackingCreator v2
    Added BootstrapVue, and tabbed packing creator was added.

--- src/components/packing/Packing.vue
+++ src/components/packing/Packing.vue
@@ -2,53 +2,36 @@
     <div class="row justify-content-center">
         <div class="col-sm-8">
             <div class="content clearfix">
-                <h3>
-                    <ribbon>Kreator Pakowania</ribbon>
-                </h3>
-                <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                    <a class="nav-link active" id="v-pills-start-tab" data-toggle="pill" href="#v-pills-start" role="tab"
-                       aria-controls="v-pills-start" aria-selected="true">Start</a>
-                    <a class="nav-link" id="v-pills-underwear-tab" data-toggle="pill" href="#v-pills-underwear" role="tab"
-                       aria-controls="v-pills-underwear" aria-selected="false">Bielizna i skarpety</a>
-                    <a class="nav-link" id="v-pills-daily-tab" data-toggle="pill" href="#v-pills-daily" role="tab"
-                       aria-controls="v-pills-daily" aria-selected="false">Ubrania codzienne</a>
-                    <a class="nav-link" id="v-pills-outerwear-tab" data-toggle="pill" href="#v-pills-outerwear" role="tab"
-                       aria-controls="v-pills-outerwear" aria-selected="false">Ubrania wierzchnie</a>
-                    <a class="nav-link" id="v-pills-other-tab" data-toggle="pill" href="#v-pills-other" role="tab"
-                       aria-controls="v-pills-other" aria-selected="false">Buty, akcesoria i inne</a>
-                    <a class="nav-link" id="v-pills-end-tab" data-toggle="pill" href="#v-pills-end" role="tab"
-                       aria-controls="v-pills-end" aria-selected="false">Koniec</a>
-                </div>
-                <div>
-                    <div class="tab-content" id="v-pills-tabContent">
-                        <div class="tab-pane fade show active" id="v-pills-start" role="tabpanel"
-                             aria-labelledby="v-pills-start-tab">
-                            <h2>Krok 1 z 3</h2>
-                            <div> Witamy w kreatorze pakowania przedmiotów w Szafiarni.</div>
-                            <form novalidate @submit.prevent="long" class="text-center">
-                                <div class="form-group">
-                                    <input v-model="tripLength" type="number" class="form-control"
-                                           placeholder="Wprowadź długość podrózy">
-                                </div>
-                                <button class="btn btn-success">Dalej</button>
-                            </form>
-                        </div>
-                        <div>
-                            <div class="tab-pane fade" id="v-pills-underwear" role="tabpanel"
-                                 aria-labelledby="v-pills-underwear-tab">...
-                            </div>
-                            <div class="tab-pane fade" id="v-pills-daily" role="tabpanel"
-                                 aria-labelledby="v-pills-daily-tab">...
-                            </div>
-                            <div class="tab-pane fade" id="v-pills-outerwear" role="tabpanel"
-                                 aria-labelledby="v-pills-outerwear-tab">...
-                            </div>
-                            <div class="tab-pane fade" id="v-pills-other" role="tabpanel"
-                                 aria-labelledby="v-pills-other-tab">...
-                            </div>
-                            <div class="tab-pane fade" id="v-pills-end" role="tabpanel" aria-labelledby="v-pills-end-tab">
-                                ...
-                            </div>
+                <h3><ribbon>Kreator Pakowania</ribbon></h3>
+                        <b-card no-body>
+                        <b-tabs pills card vertical>
+                            <b-tab title="Start" active>
+                                  <form>
+                                    <div class="form-group">
+                                        <label for="travelLength">Witamy w kreatorze pakowania przedmiotów w Szafiarni. Wprowadź liczbę dni zaplanowanego wyjazdu:</label>
+                                        <input type="number" class="form-control" id="travelLength" aria-describedby="emailHelp" placeholder="Liczba dni" required>
+                                        <small id="numberHelp" class="form-text text-muted">Liczba dni musi zostać wskazana wartością liczbową</small>
+                                    </div>
+                                  </form>
+                            </b-tab>
+                            <b-tab title="Bielizna i skarpety">
+                            Tab Contents 2
+                            </b-tab>
+                            <b-tab title="Ubrania codzienne">
+                            Tab Contents 3
+                            </b-tab>
+                            <b-tab title="Ubrania wierzchnie">
+                            Tab Contents 3
+                            </b-tab>
+                            <b-tab title="Buty, akcesoria i inne">
+                            Tab Contents 3
+                            </b-tab>
+                            <b-tab title="Koniec">
+                                <b-button>Zatwierdź wybór i przejdź do spakowanych rzeczy</b-button>
+                                <b-button>Porzuć kreator</b-button>
+                            </b-tab>
+                        </b-tabs>
+                        </b-card>
                         </div>
                     </div>
                 </div>
--- src/components/packing/component.ts
+++ src/components/packing/component.ts
@@ -1,7 +1,12 @@
 import {ContainerType} from "@/models/Container";
 import {ColourGroup, LaundryCategory, PackingCategory} from "@/models/Item";
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import Component from 'vue-class-component'
+Vue.use(BootstrapVue);
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 @Component
 export default class PackingComponent extends Vue {
