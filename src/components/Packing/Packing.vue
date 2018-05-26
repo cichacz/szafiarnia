52e2bc291602f74a2433a1f22ed3e97237568ff8
Damian Pieczara <damian1995pieczara@gmail.com>
2018-05-26 14:05:24 +0200

    Packing creator
    Little updates

--- src/components/Packing/Packing.vue
+++ src/components/Packing/Packing.vue
@@ -3,7 +3,7 @@
         <div class="col-sm-8">
             <div class="content clearfix">
                 <h3><ribbon>Kreator Pakowania</ribbon></h3>    
-                    </div>
+            </div>
                         <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                                 <a class="nav-link active" id="v-pills-start-tab" data-toggle="pill" href="#v-pills-start" role="tab" aria-controls="v-pills-start" aria-selected="true">Start</a>
                                 <a class="nav-link" id="v-pills-underwear-tab" data-toggle="pill" href="#v-pills-underwear" role="tab" aria-controls="v-pills-underwear" aria-selected="false">Bielizna i skarpety</a>
@@ -12,6 +12,7 @@
                                 <a class="nav-link" id="v-pills-other-tab" data-toggle="pill" href="#v-pills-other" role="tab" aria-controls="v-pills-other" aria-selected="false">Buty, akcesoria i inne</a>
                                 <a class="nav-link" id="v-pills-end-tab" data-toggle="pill" href="#v-pills-end" role="tab" aria-controls="v-pills-end" aria-selected="false">Koniec</a>
                         </div>
+                        <div>
                                 <div class="tab-content" id="v-pills-tabContent">
                                 <div class="tab-pane fade show active" id="v-pills-start" role="tabpanel" aria-labelledby="v-pills-start-tab">
                                     <h2>Krok 1 z 3</h2>
@@ -21,19 +22,17 @@
                                                 <input v-model="text" type="text" class="form-control" placeholder="Wprowadź długość podrózy">
                                             </div>
                                         <button class="btn btn-success">Dalej</button>
-                                        
-                                        </form>
-                                </div>
-                                <div>
-                                <div class="tab-pane fade" id="v-pills-underwear" role="tabpanel" aria-labelledby="v-pills-underwear-tab">...</div>
-                                <div class="tab-pane fade" id="v-pills-daily" role="tabpanel" aria-labelledby="v-pills-daily-tab">...</div>
-                                <div class="tab-pane fade" id="v-pills-outerwear" role="tabpanel" aria-labelledby="v-pills-outerwear-tab">...</div>
-                                <div class="tab-pane fade" id="v-pills-other" role="tabpanel" aria-labelledby="v-pills-other-tab">...</div>
-                                <div class="tab-pane fade" id="v-pills-end" role="tabpanel" aria-labelledby="v-pills-end-tab">...</div>
-
-            </div>                   
-        </div>
-    </div>   
+                                        </form></div>
+                                    <div>
+                                    <div class="tab-pane fade" id="v-pills-underwear" role="tabpanel" aria-labelledby="v-pills-underwear-tab">...</div>
+                                    <div class="tab-pane fade" id="v-pills-daily" role="tabpanel" aria-labelledby="v-pills-daily-tab">...</div>
+                                    <div class="tab-pane fade" id="v-pills-outerwear" role="tabpanel" aria-labelledby="v-pills-outerwear-tab">...</div>
+                                    <div class="tab-pane fade" id="v-pills-other" role="tabpanel" aria-labelledby="v-pills-other-tab">...</div>
+                                    <div class="tab-pane fade" id="v-pills-end" role="tabpanel" aria-labelledby="v-pills-end-tab">...</div>
+                                    </div>  
+                                </div>                 
+                        </div>
+        </div>   
 </template>
 <script src="./component.ts" lang="ts"></script>
 <style src="./style.less" scoped lang="less"></style>
--- src/components/Packing/component.ts
+++ src/components/Packing/component.ts
@@ -5,56 +5,6 @@ import Component from 'vue-class-component'
 import {Prop} from "vue-property-decorator";
 
 @Component
-export default class ItemComponent extends Vue {
-  @Prop()
-  id: string;
-
-  @Prop()
-  container: string;
-
-  @Prop()
-  saved: string;
-
-  updated: boolean = false;
-
-  get formTitle() {
-    return this.currentItem.name.length ? this.currentItem.name : 'Dodaj nowy przedmiot';
-  }
-
-  public currentItem: Item = new Item('');
-  public error: string = '';
-
-  public colorGroup = ColourGroup;
-  public laundryCategory = LaundryCategory;
-  public packingCategory = PackingCategory;
-
-  async created() {
-    if(this.id) {
-      const doc = await this.$dao.getItemById(this.id, this.container);
-      if(doc) {
-        this.currentItem = doc;
-      }
-    }
-  }
-
-  addItem() {
-    if(!this.currentItem.idContainer) {
-      let defaultContainer = this.$store.state.containers.list.find((el: Container) => el.type == ContainerType.Default);
-      if(defaultContainer) {
-        this.currentItem.idContainer = defaultContainer.id;
-      }
-    }
-
-    this.$dao.saveItem(this.currentItem).then((doc: any) => {
-      if(doc) {
-        this.$router.push({name: 'item', params: {id: doc.id, saved: "1"}});
-      } else {
-        this.updated = true;
-      }
-    });
-  }
-
-  cancel() {
-    this.$router.go(-1);
-  }
+export default class PackingComponent extends Vue {
+ 
 }
